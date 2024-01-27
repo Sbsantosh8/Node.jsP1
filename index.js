94bf31faeef3543c884f463efbd2918c59627f1c
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2024-01-27 11:09:46 +0530

    Adding

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-// NPM module
+// // NPM module
 
-const clc = require('cli-color');
+// const clc = require('cli-color');
 
-// Example: Print colored text to the console
-console.log(clc.green('This is green text!'));
-console.log(clc.red.bold('This is bold red text!'));
+// // Example: Print colored text to the console
+// console.log(clc.green('This is green text!'));
+// console.log(clc.red.bold('This is bold red text!'));
 
 
-// Local module
+// // Local module
 
 // const register1 = require('./auth');
 // register1("Santosh");
 
+// const login1 = require('./auth');
 
-const login1 = require('./auth');
-
-login1.register("Santosh")
-login1.login("Santya","tumDono ki maki choot")
+// login1.register1("Santosh")
+// login1.login("Santya","tumDono ki maki choot")
 
 
 
 // Core Module
 
+// Dirname
 const path = require('path');
 
 console.log('Folder name: ',path.dirname(__filename));
+
+
+// Filename
+
+console.log('File name: ',path.basename(__filename));
+
+// Extension
+
+console.log('Extension: ',path.extname(__filename))
+
+// Parse
+
+console.log('Parse :', path.parse(__filename))
+
+// Join
+
+console.log('Join: ',path.join(__dirname,'order','app.js'))
