3d569f78b65d7541261a4cb68bc32f5537411bfe
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2024-01-27 09:45:25 +0530

    Adding a index.js and auth.js file

---
+++ auth.js
@@ -0,0 +1,13 @@
+const register = function(username){
+    console.log(`User ${username} has been registered`)
+}
+
+const login = function(username,password){
+    console.log(`User ${username} is logged in`);
+}
+// module.exports = register;
+ 
+module.exports ={
+    register1 : register,
+    login : login
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+const clc = require('cli-color');
+
+// Example: Print colored text to the console
+console.log(clc.green('This is green text!'));
+console.log(clc.red.bold('This is bold red text!'));
+
+
+// Local module
+
+// const register1 = require('./auth');
+// register1("Santosh");
+
+
+const login1 = require('./auth');
+
+login1.register("Santosh")
+login1.login("Santya","tumDono ki maki choot")
+// login1(
+// 
